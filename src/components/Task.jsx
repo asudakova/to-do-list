5c4080182323abf5467e92975597a5ab23c8dd5e
asudakova <thesashek@yandex.ru>
2022-11-18 21:11:10 +0300

    added line-through when the task is done

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import circle_btn from "../img/circle.svg";
+import check_btn from "../img/check-mark.svg";
 import trashcan_btn from "../img/trashcan.svg";
 
 const Task = ({children}) => {
+  const [isDone, setIsDone] = useState(false)
+  const switchIsDone = () => {
+    return isDone ? setIsDone(false) : setIsDone(true)
+  }
   return (
     <li className="App__task">
-      <img src={circle_btn} alt="Circle btn"/>
-      <p>{children}</p>
+      <img onClick={switchIsDone} src={isDone ? check_btn : circle_btn} alt="Circle btn"/>
+      <p className={isDone ? 'done' : ''}>{children}</p>
       <img src={trashcan_btn} alt="Trashcan btn"/>
     </li>
   );
