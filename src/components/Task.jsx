1d99256ff0efd71cd70756d2af3f580c3c54b2c2
asudakova <thesashek@yandex.ru>
2022-11-19 13:23:59 +0300

    added removing a chosen task from the list

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,16 +3,20 @@ import circle_btn from "../img/circle.svg";
 import check_btn from "../img/check-mark.svg";
 import trashcan_btn from "../img/trashcan.svg";
 
-const Task = ({children}) => {
+const Task = ({task, remove}) => {
   const [isDone, setIsDone] = useState(false)
+
   const switchIsDone = () => {
-    return isDone ? setIsDone(false) : setIsDone(true)
+    isDone ? setIsDone(false) : setIsDone(true)
+
+    return isDone
   }
+
   return (
     <li className="App__task">
       <img onClick={switchIsDone} src={isDone ? check_btn : circle_btn} alt="Circle btn"/>
-      <p className={isDone ? 'done' : ''}>{children}</p>
-      <img src={trashcan_btn} alt="Trashcan btn"/>
+      <p className={isDone ? 'done' : ''}>{task.text}</p>
+      <img onClick={() => remove(task)} src={trashcan_btn} alt="Trashcan btn"/>
     </li>
   );
 };
